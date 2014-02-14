4bcc88c3d8cd487ee75ffa642c6e3123b4331dc6
Gab-km <the25thcromosome@gmail.com>
2014-02-14 13:44:20 +0900

    Modify typos

--- tutorial/views.py
+++ tutorial/views.py
@@ -17,28 +17,28 @@ wikiwords = re.compile(r"\b([A-Z]\w+[A-Z]+\w+)")
 
 @view_config(route_name='view_wiki')
 def view_wiki(request):
-    return HTTPFound(location=request.rout_url('view_page', pagename='FrontPage'))
+    return HTTPFound(location=request.route_url('view_page', pagename='FrontPage'))
 
 @view_config(route_name='view_page', renderer='templates/view.pt')
 def view_page(request):
     pagename = request.matchdict['pagename']
-    page = DBSession.query_property(Page).filter_by(name=pagename).first()
+    page = DBSession.query(Page).filter_by(name=pagename).first()
     if page is None:
         return HTTPNotFound('No such page')
 
     def check(match):
         word = match.group(1)
-        exists = DBSession.query_property(Page).filter_by(name=word).all()
+        exists = DBSession.query(Page).filter_by(name=word).all()
         if exists:
-            view_url = request.rout_url('view_page', pagename=word)
+            view_url = request.route_url('view_page', pagename=word)
             return '<a href="{0}">{1}</a>'.format(view_url, word)
         else:
-            add_url = request.rout_url('add_page', pagename=word)
+            add_url = request.route_url('add_page', pagename=word)
             return '<a href="{0}">{1}</a>'.format(add_url, word)
 
     content = publish_parts(page.data, writer_name='html')['html_body']
     content = wikiwords.sub(check, content)
-    edit_url = request.rout_url('edit_page', pagename=pagename)
+    edit_url = request.route_url('edit_page', pagename=pagename)
     return dict(page=page, content=content, edit_url=edit_url)
 
 @view_config(route_name='add_page', renderer='templates/edit.pt')
@@ -48,17 +48,17 @@ def add_page(request):
         body = request.params['body']
         page = Page(pagename, body)
         DBSession.add(page)
-        return HTTPFound(location=request.rout_url('view_page', pagename=pagename))
-    save_url = request.rout_url('add_page', pagename=pagename)
+        return HTTPFound(location=request.route_url('view_page', pagename=pagename))
+    save_url = request.route_url('add_page', pagename=pagename)
     page = Page('', '')
     return dict(page=page, save_url=save_url)
 
 @view_config(route_name='edit_page', renderer='templates/edit.pt')
 def edit_page(request):
     pagename = request.matchdict['pagename']
-    page = DBSession.query_property(Page).filter_by(name=pagename).one()
+    page = DBSession.query(Page).filter_by(name=pagename).one()
     if 'form.submitted' in request.params:
         page.data = request.params['body']
         DBSession.add(page)
-        return HTTPFound(location=request.rout_url('view_page', pagename=pagename))
-    return dict(page=page, save_url=request.rout_url('edit_page', pagename=pagename),)
+        return HTTPFound(location=request.route_url('view_page', pagename=pagename))
+    return dict(page=page, save_url=request.route_url('edit_page', pagename=pagename),)
